c14fc230a8481f5bf0a6d30df51ea243444582bc
David Girou <david.girou@0Davgi0.com>
2024-05-13 13:55:22 +0200

    Fixed issue with key handling for submenus

--- renderer.js
+++ renderer.js
@@ -1,110 +1,134 @@
 // renderer.js
+let currentTab = 'stat';  // Keep track of the current tab
+let keyDownListener;  // Reference to the global keydown listener
+
+export function setKeyDownListener(newListener) {
+    if (keyDownListener) {
+        document.removeEventListener('keydown', keyDownListener);
+    }
+    keyDownListener = newListener;
+    document.addEventListener('keydown', keyDownListener);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const menuItems = document.querySelectorAll('#menu .nav-item');
     
     menuItems.forEach(item => {
-      item.addEventListener('click', () => {
-        setActiveTab(item.dataset.tab);
-      });
+        item.addEventListener('click', () => {
+            setActiveTab(item.dataset.tab);
+        });
     });
     
     // Set initial active tab
     setActiveTab('stat');
-  });
-  
-  function setActiveTab(tab) {
+});
+
+function setActiveTab(tab) {
+    // Cleanup if moving away from 'inv'
+    if (currentTab === 'inv') {
+        if (keyDownListener) {
+            document.removeEventListener('keydown', keyDownListener);
+            keyDownListener = null;
+        }
+    }
+
     // Clear active state for all items
     const menuItems = document.querySelectorAll('#menu .nav-item');
     menuItems.forEach(item => item.classList.remove('active'));
-  
+
     // Set active state on selected item
     const activeItem = document.querySelector(`#menu .nav-item[data-tab="${tab}"]`);
     if (activeItem) {
-      activeItem.classList.add('active');
+        activeItem.classList.add('active');
     }
-  
+
     // Load the selected tab content
     loadTabContent(tab);
-  }
-  
-  function loadTabContent(tab) {
+    currentTab = tab;  // Update the current tab
+}
+
+function loadTabContent(tab) {
     fetch(`tabs/${tab}.html`)
-      .then(response => response.text())
-      .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const template = doc.querySelector('template');
-        const content = document.importNode(template.content, true);
-        const interfaceDiv = document.getElementById('interface');
-  
-        // Clear existing content and append the new tab content
-        interfaceDiv.innerHTML = '';
-        interfaceDiv.appendChild(content);
+        .then(response => response.text())
+        .then(html => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+            const template = doc.querySelector('template');
+            const content = document.importNode(template.content, true);
+            const interfaceDiv = document.getElementById('interface');
 
-        // Re-initialize the slider functionality if necessary
-        if (tab === "settings") { // Assuming the hue-slider is only in the 'settings' tab
-          initializeColorSliders();
-        }
-      })
-      .catch(error => console.error(`Error loading ${tab} content:`, error));
+            interfaceDiv.innerHTML = '';
+            interfaceDiv.appendChild(content);
+
+            if (tab === "inv") {
+                import('./subMenus.js').then(module => {
+                    module.initializeSubMenuActions();
+                });
+            }
+
+            // Re-initialize the slider functionality if necessary
+            if (tab === "settings") { // Assuming the hue-slider is only in the 'settings' tab
+              initializeColorSliders();
+            }
+        })
+        .catch(error => console.error(`Error loading ${tab} content:`, error));
+}
+
+function initializeColorSliders() {
+  const hueSlider = document.getElementById('hue-slider');
+  const satSlider = document.getElementById('sat-slider');
+  const lightSlider = document.getElementById('light-slider');
+
+  function updateColor() { // TODO: update the sliders to the current values
+      const hueValue = hueSlider.value;
+      const satValue = satSlider.value;
+      const lightValue = lightSlider.value;
+      const newLight = `hsl(${hueValue}, ${satValue}%, ${lightValue}%)`;
+      const newMed = `hsla(${hueValue}, ${satValue}%, ${lightValue}%, 0.2)`;
+      const newDark = `hsla(${hueValue}, ${satValue}%, ${lightValue}%, 0.067)`;
+      document.documentElement.style.setProperty('--hue', `${hueSlider.value-140}deg`);
+      document.documentElement.style.setProperty('--sat', `${satSlider.value}%`);
+      document.documentElement.style.setProperty('--brightness', `${lightSlider.value*2}%`);
+      document.documentElement.style.setProperty('--light', newLight);
+      document.documentElement.style.setProperty('--medium', newMed);
+      document.documentElement.style.setProperty('--dark', newDark);
   }
 
-  function initializeColorSliders() {
-    const hueSlider = document.getElementById('hue-slider');
-    const satSlider = document.getElementById('sat-slider');
-    const lightSlider = document.getElementById('light-slider');
+  // Add event listeners to all sliders
+  hueSlider.addEventListener('input', updateColor);
+  satSlider.addEventListener('input', updateColor);
+  lightSlider.addEventListener('input', updateColor);
+}
 
-    function updateColor() {
-        const hueValue = hueSlider.value;
-        const satValue = satSlider.value;
-        const lightValue = lightSlider.value;
-        const newLight = `hsl(${hueValue}, ${satValue}%, ${lightValue}%)`;
-        const newMed = `hsla(${hueValue}, ${satValue}%, ${lightValue}%, 0.2)`;
-        const newDark = `hsla(${hueValue}, ${satValue}%, ${lightValue}%, 0.067)`;
-        document.documentElement.style.setProperty('--hue', `${hueSlider.value-140}deg`);
-        document.documentElement.style.setProperty('--sat', `${satSlider.value}%`);
-        document.documentElement.style.setProperty('--brightness', `${lightSlider.value*2}%`);
-        document.documentElement.style.setProperty('--light', newLight);
-        document.documentElement.style.setProperty('--medium', newMed);
-        document.documentElement.style.setProperty('--dark', newDark);
-    }
 
-    // Add event listeners to all sliders
-    hueSlider.addEventListener('input', updateColor);
-    satSlider.addEventListener('input', updateColor);
-    lightSlider.addEventListener('input', updateColor);
+document.addEventListener('keydown', (event) => {
+  handleKeyEvent(event.code);
+});
+
+function handleKeyEvent(keyCode) {
+  switch (keyCode) {
+    case 'Digit1':
+      setActiveTab('stat');
+      break;
+    case 'Digit2':
+      setActiveTab('inv');
+      break;
+    case 'Digit3':
+      setActiveTab('data');
+      break;
+    case 'Digit4':
+      setActiveTab('map');
+      break;
+    case 'Digit5':
+      setActiveTab('radio');
+      break;
+    case 'ArrowLeft':
+      console.log('Scroll Knob Turned Left');
+      break;
+    case 'ArrowRight':
+      console.log('Scroll Knob Turned Right');
+      break;
+    default:
+      break;
   }
-
-  
-  document.addEventListener('keydown', (event) => {
-    handleKeyEvent(event.code);
-  });
-  
-  function handleKeyEvent(keyCode) {
-    switch (keyCode) {
-      case 'Digit1':
-        setActiveTab('stat');
-        break;
-      case 'Digit2':
-        setActiveTab('inv');
-        break;
-      case 'Digit3':
-        setActiveTab('data');
-        break;
-      case 'Digit4':
-        setActiveTab('map');
-        break;
-      case 'Digit5':
-        setActiveTab('radio');
-        break;
-      case 'ArrowLeft':
-        console.log('Scroll Knob Turned Left');
-        break;
-      case 'ArrowRight':
-        console.log('Scroll Knob Turned Right');
-        break;
-      default:
-        break;
-    }
-  }
-  
+}
--- subMenus.js
+++ subMenus.js
@@ -1,57 +1,62 @@
-//document.addEventListener('DOMContentLoaded', () => {
-//const submenuItems = document.querySelectorAll('.sub-nav-item');
+// subMenus.js
+import { setKeyDownListener } from './renderer.js';
 
-// Set initial active state and center the first item
-setActiveAndCenter((document.querySelectorAll('.sub-nav-item'))[0]);  // Assuming the first item should be active
-
-// Add click event listeners to all submenu items
-(document.querySelectorAll('.sub-nav-item')).forEach(item => {
-    item.addEventListener('click', function() {
-        setActiveAndCenter(this);
+export function initializeSubMenuActions() {
+    const submenuItems = document.querySelectorAll('.sub-nav-item');
+    
+    submenuItems.forEach(item => {
+        item.removeEventListener('click', setActiveAndCenterHandler); // Remove old listener to avoid duplicates
+        item.addEventListener('click', setActiveAndCenterHandler); // Add new listener
     });
-});
 
-// Attach the keydown listener only once to the document
-document.addEventListener('keydown', (event) => {
-    handleArrowKeys(event, (document.querySelectorAll('.sub-nav-item')));
-});
-//});
-
-function handleArrowKeys(event, submenuItems) {
-const activeItem = document.querySelector('.sub-nav-item.active');
-let newIndex;
-switch (event.key) {
-    case 'ArrowRight':
-        newIndex = (Array.from(submenuItems).indexOf(activeItem) + 1) % submenuItems.length;
-        setActiveAndCenter(submenuItems[newIndex]);
-        break;
-    case 'ArrowLeft':
-        newIndex = (Array.from(submenuItems).indexOf(activeItem) - 1 + submenuItems.length) % submenuItems.length;
-        setActiveAndCenter(submenuItems[newIndex]);
-        break;
-}
+    setKeyDownListener((event) => handleArrowKeys(event, submenuItems));
+    setActiveAndCenter(submenuItems[0])
 }
 
-function setActiveAndCenter(selectedItem) {
-const submenuItems = document.querySelectorAll('.sub-nav-item');
-submenuItems.forEach(item => {
-    item.classList.remove('active');
-    item.style.opacity = "0.5";  // Reset opacity
-});
-
-selectedItem.classList.add('active');
-selectedItem.style.opacity = "1";
-
-const submenu = document.querySelector('.submenu');
-const submenuWidth = submenu.offsetWidth;
-const itemCenter = selectedItem.offsetLeft + selectedItem.offsetWidth / 2;
-const offset = -(itemCenter - submenuWidth / 3.5);
-submenu.style.transform = `translateX(${offset}px)`;
-
-const activeIndex = Array.from(submenuItems).indexOf(selectedItem);
-submenuItems.forEach((item, index) => {
-    const distance = Math.abs(index - activeIndex);
-    const opacity = Math.max(0.01, 1 - 0.35 * distance);
-    item.style.opacity = opacity.toString();
-});
+// Define a global function to handle arrow key navigation
+export function handleArrowKeys(event, submenuItems) {
+    const activeItem = document.querySelector('.sub-nav-item.active');
+    let newIndex;
+    switch (event.key) {
+        case 'ArrowRight':
+            newIndex = (Array.from(submenuItems).indexOf(activeItem) + 1) % submenuItems.length;
+            setActiveAndCenter(submenuItems[newIndex]);
+            break;
+        case 'ArrowLeft':
+            newIndex = (Array.from(submenuItems).indexOf(activeItem) - 1 + submenuItems.length) % submenuItems.length;
+            setActiveAndCenter(submenuItems[newIndex]);
+            break;
+    }
 }
+
+function setActiveAndCenterHandler(event) {
+    setActiveAndCenter(event.currentTarget);  // Pass currentTarget instead of the event object
+}
+
+// Define a function to set the active submenu item and adjust view as needed
+export function setActiveAndCenter(selectedItem) {
+    const submenuItems = document.querySelectorAll('.sub-nav-item');
+    submenuItems.forEach(item => {
+        item.classList.remove('active');
+        item.style.opacity = "0.5"; // Reset opacity
+    });
+
+    console.log(selectedItem);
+    console.log(selectedItem.classList);
+    selectedItem.classList.add('active');
+    selectedItem.style.opacity = "1";
+
+    const submenu = document.querySelector('.submenu');
+    const submenuWidth = submenu.offsetWidth;
+    const itemCenter = selectedItem.offsetLeft + selectedItem.offsetWidth / 2;
+    const offset = -(itemCenter - submenuWidth / 3.5);
+    submenu.style.transform = `translateX(${offset}px)`;
+
+    const activeIndex = Array.from(submenuItems).indexOf(selectedItem);
+    submenuItems.forEach((item, index) => {
+        const distance = Math.abs(index - activeIndex);
+        const opacity = Math.max(0.01, 1 - 0.35 * distance);
+        item.style.opacity = opacity.toString();
+    });
+}
+
