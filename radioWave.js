a6f82973e7e280187e3b0c420b7cf97843319cfb
David Girou <david.girou@0Davgi0.com>
2024-05-27 17:47:42 +0200

    Adjusted stroke sizes and graduations on the radiowaves graph

--- radioWave.js
+++ radioWave.js
@@ -38,7 +38,7 @@ export function radioWave() {
     waveClone.setAttribute('d', wavePathData);
     waveClone.setAttribute('fill', 'none');
     waveClone.setAttribute('stroke', 'var(--light)');
-    waveClone.setAttribute('stroke-width', '2');
+    waveClone.setAttribute('stroke-width', '3');
     waveClone.setAttribute('transform', 'translate(400, 0)');
     document.getElementById('wave-path').appendChild(waveClone);
 
@@ -50,7 +50,7 @@ export function radioWave() {
         for (let y = 0; y <= 400; y += 10) {
             if (y >= 400) continue;
             const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-            line.setAttribute('x1', y % 50 === 0 ? 393 : 396); // Longer graduation for multiples of 50
+            line.setAttribute('x1', y % 50 === 0 ? 389 : 393); // Longer graduation for multiples of 50
             line.setAttribute('x2', 400);
             line.setAttribute('y1', y);
             line.setAttribute('y2', y);
@@ -66,7 +66,7 @@ export function radioWave() {
             line.setAttribute('x1', x);
             line.setAttribute('x2', x);
             line.setAttribute('y1', 400);
-            line.setAttribute('y2', x % 50 === 0 ? 393 : 396); // Longer graduation for multiples of 50
+            line.setAttribute('y2', x % 50 === 0 ? 389 : 393); // Longer graduation for multiples of 50
             line.setAttribute('stroke', 'var(--light)');
             line.setAttribute('stroke-width', '2');
             horizontalGraduations.appendChild(line);
